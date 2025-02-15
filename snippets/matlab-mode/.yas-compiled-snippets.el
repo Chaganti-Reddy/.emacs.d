cf84fccfcb5171624a56aaf6a848181a60b90920
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-15 21:30:48 +0530

    create snippets/matlab-mode/.yas-compiled-snippets.el

---
+++ snippets/matlab-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `matlab-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'matlab-mode
+		     '(("switch" "switch ${1:variable}\n    case ${2:value}\n    $3\n    otherwise\nend\n    " "switch" nil nil nil "/home/karna/.emacs.d/snippets/matlab-mode/switch" nil nil)))
+
+
+;;; Do not edit! File generated at Sat Feb 15 12:32:41 2025
