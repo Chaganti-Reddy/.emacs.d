b3ee6d68ff0ec23fa9bc1da02209dedadd6560b6
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-21 21:02:06 +0530

    Delete .yas-compiled-snippets.el

--- snippets/eshell-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `eshell-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'eshell-mode
-		     '(("for" "for f in ${1:*} { ${2:echo} \"$f\"; $3} $0" "Eshell for loop" nil nil nil "/home/karna/.emacs.d/snippets/eshell-mode/for" nil nil)))
-
-
-;;; Do not edit! File generated at Sat Feb 15 12:32:41 2025
