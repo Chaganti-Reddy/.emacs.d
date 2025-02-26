2faea4b2a09a174f39692238ad36a565b6adc25a
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-26 16:52:45 +0530

    create lisp/setup-yas.el

---
+++ lisp/setup-yas.el
@@ -0,0 +1,28 @@
+;;; setup-yas.el --- Yasnippet Configurations -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Yasnippet - Snippet system for emacs configurations
+
+;;; Code:
+
+(use-package yasnippet
+  :ensure t
+  :hook ((prog-mode . yas-minor-mode)
+	 (text-mode . yas-minor-mode)
+	 (LaTeX-mode . yas-minor-mode))
+  :custom
+  (yas-snippet-dirs '("~/.emacs.d/snippets/")) ;; Custom snippet directory
+  (yas-triggers-in-field t) ;; Allow nested expansion inside fields
+  :config
+  (yas-reload-all))
+
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet
+  :config
+  (yas-reload-all))
+
+
+(provide 'setup-yas)
+;;; setup-yas.el ends here
