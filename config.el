f0913ef0c4f6459af5d7c1e0aa1c3d0699e65059
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-26 16:52:44 +0530

    update config.el

--- config.el
+++ config.el
@@ -1,49 +1,32 @@
-;;; ORG TEMPO
-
-(require 'org-tempo)
-
-;;; $EMACSDIR/config.el --- Emacs Setup -*- lexical-binding: t; -*-
-
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-
-(require 'elpaca)  ;; The Elpaca Package Manager
-(require 'buffer-move) ;; Buffer Moving Custom Functions
-(require 'hooks-dirs) ;; Hooks and Directories
-(require 'core-config) ;; Core Configurations
-(require 'editor) ;; Editor Settings
-(require 'packages/evil) ;; Evil Mode
-(require 'ui-config) ;; UI Settings for Emacs
-(require 'keybindings) ;; General Custom Keybindings
-(require 'git) ;; Git
-(require 'calcc) ;; Calc
-(require 'file-manager) ;; File Manager
-(require 'packages/wakatime) ;; Wakatime
-(require 'packages/dashboard) ;; Dashboard
-(require 'packages/doom-modeline) ;; Doom Modeline
-(require 'packages/projectile) ;; Projectile
-(require 'packages/perspective) ;; Perspective
-(require 'packages/minibuffer) ;; Minibuffer
-(require 'packages/consult) ;; Consult
-(require 'packages/corfu) ;; Corfu
-(require 'packages/cape) ;; Cape
-(require 'packages/orderless) ;; Orderless
-(require 'org-config) ;; Org Config
-(require 'org-agenda-config) ;; Org Agenda
-(require 'org-roam-config) ;; Org Roam
-(require 'packages/tabnine) ;; TabNine
-(require 'packages/yasnippet) ;; Yasnippet
-(require 'packages/gptel) ;; GPTel
-(require 'treesitter) ;; Treesitter
-(require 'dev) ;; Development Stuff
-(require 'packages/conda) ;; Conda
-(require 'packages/pyenv) ;; PyEnv
-(require 'packages/latex) ;; Latex
-(require 'packages/org-latex) ;; Org Latex
-(require 'packages/markdown) ;; Markdown
-(require 'eshell-prompt) ;; Custom ESHELL Prompt
-(require 'shells) ;; Shells
-(require 'packages/calendar) ;; Calendar
-(require 'consult-yasnippet) ;; Consult Yasnippet
-
-(provide 'config)
-;;; config.el ends here
+(require 'optimizations) ; Load Emacs optimizations config file
+(require 'core-config) ; Load the core configurations file
+(require 'editor) ;; Editor settings config file
+(require 'buffer-move) ;; Move Buffers accross emacs
+(require 'shells) ;; Terminal Setup
+(require 'eshell-prompt) ;; Eshell prompt config
+(require 'evil-mode-config) ;;  Evil mode configurations file
+(require 'ui-config) ;; UI configuration for emacs
+(require 'dashboard-config) ;; Dashboard and modeline config for emacs
+(require 'popups) ;; Popper configuration for emacs buffers
+(require 'projects) ;; Project configuration
+(require 'workspace) ;; Perspective.el config
+(require 'minibuffer-ui) ;; Minibuffer UI configurations
+(require 'consult-project-extra) ;; Consult integration for project.el
+;; (require 'setup-corfu) ;; Corfu Configuration
+;; (require 'setup-cape) ;; Cape Configuration
+(require 'setup-orderless) ;; Orderless Configuration
+(require 'setup-completions-default) ;; Builtin completions
+;; (require 'tabnine-config) ;; Tabnine inline AI completion system
+(require 'setup-yas) ;; Yasnippet configurations
+(require 'consult-yasnippet) ;; Consult integration for Yasnippet
+(require 'setup-git) ;; Git Configuration
+(require 'setup-gptel) ;; GPTEL config
+(require 'setup-calc) ;; Emacs CALC config
+(require 'setup-dired) ;; Dired config
+(require 'setup-treesit) ;; Tressiter Configuration
+(require 'setup-eglot) ;; Eglot Setup
+(require 'setup-doc) ;; PDF Tools
+(require 'setup-tex) ;; LaTeX setup
+(require 'setup-md) ;; Markdown Setup
+(require 'setup-org) ;; Org mode Configurations
+(require 'setup-calendar) ;; Calendar Configurations
