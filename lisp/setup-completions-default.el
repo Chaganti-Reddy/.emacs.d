fc20e83544c4da1906cc165a5a1deb2fe1a287b6
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-26 16:52:45 +0530

    create lisp/setup-completions-default.el

---
+++ lisp/setup-completions-default.el
@@ -0,0 +1,12 @@
+;;; setup-completions-default.el --- Completion System Configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Completion system using Emacs 30 built in completion system
+
+;;; Code:
+
+
+
+(provide 'setup-completions-default)
+
+;;; setup-completions-default.el ends here
