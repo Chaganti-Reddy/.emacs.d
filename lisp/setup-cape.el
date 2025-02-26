73cbeb150d634387463db5da26f268b3fe6f1e52
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-26 16:52:45 +0530

    create lisp/setup-cape.el

---
+++ lisp/setup-cape.el
@@ -0,0 +1,22 @@
+;;; setup-cape.el --- Completion At Point Extensions (CAPE) -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This file configures CAPE (Completion At Point Extensions) to enhance Emacs completion.
+
+;;; Code:
+
+(use-package cape
+  :ensure t
+  :init
+  (dolist (hook '((prog-mode-hook . (cape-keyword cape-dabbrev cape-file))
+                  (emacs-lisp-mode-hook . (cape-elisp-symbol cape-elisp-block cape-file))
+                  (org-mode-hook . (cape-dabbrev cape-keyword cape-abbrev cape-file))
+                  (LaTeX-mode-hook . (cape-tex cape-dabbrev cape-keyword cape-file))
+                  (sgml-mode-hook . (cape-sgml cape-dabbrev cape-file))
+                  (text-mode-hook . (cape-dabbrev cape-abbrev cape-file))))
+    (dolist (fn (cdr hook))
+      (add-hook (car hook) (lambda ()
+                             (add-hook 'completion-at-point-functions fn 'append))))))
+
+(provide 'setup-cape)
+;;; setup-cape.el ends here
