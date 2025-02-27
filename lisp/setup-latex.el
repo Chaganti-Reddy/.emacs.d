6aaf028b7404c0d50a88d68a609b0231a2eaca2e
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-27 22:16:02 +0530

    create lisp/setup-latex.el

---
+++ lisp/setup-latex.el
@@ -0,0 +1,203 @@
+;;; setup-latex.el --- LaTeX & AUCTeX Setup -*- lexical-binding: t; -*-
+
+;; ----------------------------------------------------------------------------
+;; AUCTeX Setup
+;; ----------------------------------------------------------------------------
+
+(use-package tex
+  :ensure auctex 
+  :defer t
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :hook ((LaTeX-mode . LaTeX-math-mode)
+         (LaTeX-mode . visual-line-mode)
+         (LaTeX-mode . electric-pair-mode)
+         (LaTeX-mode . prettify-symbols-mode)
+         (LaTeX-mode . rainbow-delimiters-mode)
+         (LaTeX-mode . my/latex-with-outline)
+         (LaTeX-mode . TeX-fold-mode))
+  :custom
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (TeX-electric-escape nil)
+  (TeX-PDF-mode t)
+  (TeX-save-query nil)
+  (TeX-show-compilation nil)
+  (reftex-plug-into-AUCTeX t)
+  (LaTeX-indent-level 2)
+  (LaTeX-item-indent 2)
+  (TeX-electric-math '("$" . "$"))
+  ;; (preview-scale-function 1.2)
+  (preview-auto-cache-preamble t)
+  (preview-default-option-list '("displaymath" "graphics" "textmath"
+                                 "footnotes" "sections" "showlabels"
+                                 "psfixbb" "floats" "tikzpicture"))
+  (TeX-master
+   (lambda ()
+     (let ((default-master (expand-file-name "main.tex"
+                                             (or (file-name-directory (buffer-file-name))
+                                                 default-directory))))
+       (if (file-exists-p default-master)
+           "main.tex"
+         (progn
+           (message "main.tex not found, please select a master file")
+           (read-file-name "Choose master file: " nil nil t))))))
+  :config
+  (defun my/latex-with-outline ()
+      (add-to-list 'minor-mode-overriding-map-alist
+                   `(outline-minor-mode . ,outline-minor-mode-map))
+      (outline-minor-mode 1))
+  (defun TeX-insert-smallmatrix () (interactive)
+         (insert "[\\begin{smallmatrix}  \\end{smallmatrix}]")
+         (backward-char 19))
+  (defun TeX-insert-bmatrix () (interactive)
+         (insert "\\begin{bmatrix}  \\end{bmatrix}")
+         (backward-char 14))
+  (with-eval-after-load 'latex
+      (define-key LaTeX-mode-map (kbd "M-RET") 'LaTeX-insert-item))
+
+  (with-eval-after-load 'latex
+    (define-key TeX-mode-map [C-down-mouse-1] 'TeX-view-mouse))
+
+  (setq-default TeX-master nil))
+
+;; ----------------------------------------------------------------------------
+;; Function to Clear LaTeX Build Files
+;; ----------------------------------------------------------------------------
+
+(defun karna/clear-latex-build ()
+  "Remove all LaTeX compilation files except .tex and .pdf."
+  (interactive)
+  (when (yes-or-no-p "Delete all LaTeX build files except .tex and .pdf?")
+    (call-process "/bin/sh" nil nil nil "-c"
+                  "rm -rf auto *.prv *.fmt *.aux *.bbl *.blg *.log *.out *.toc *.lof *.lot *.synctex.gz *.fls *.fdb_latexmk _region_.tex")
+    (message "LaTeX build files deleted.")))
+
+(use-package latex-extra
+  :disabled
+  :after latex
+  :defines (latex-extra-mode)
+  :hook (LaTeX-mode . latex-extra-mode))
+
+;; ----------------------------------------------------------------------------
+;; Zathura PDF Viewer Setup with SyncTeX
+;; ----------------------------------------------------------------------------
+
+(with-eval-after-load 'latex
+  (add-to-list 'TeX-view-program-list
+               `("Zathura"
+                 (,(concat (expand-file-name "~/.local/bin/zathura")
+                           " --synctex-forward %n:0:%b -x \"emacsclient +%{line} %{input}\" %o"))
+                 "zathura"))
+  (setq-default TeX-source-correlate-mode t)
+  (setq-default TeX-source-correlate-start-server t)
+  (setq TeX-newline-function 'reindent-then-newline-and-indent)
+  (setq TeX-view-program-selection '((output-pdf "Zathura"))
+        TeX-source-correlate-method 'synctex))
+
+(add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+
+(use-package preview
+  :ensure nil
+  :defer
+  :after latex
+  :hook (LaTeX-mode . my/preview-scale-larger)
+  :config
+  (define-key LaTeX-mode-map (kbd "C-c C-x") preview-map)
+  (defun my/preview-scale-larger ()
+    "Increase the size of `preview-latex' images"
+    (setq preview-scale-function 
+          (lambda nil (* 1.25 (funcall (preview-scale-from-face)))))))
+
+;; ----------------------------------------------------------------------------
+;; RefTeX - Reference & Citation Management
+;; ----------------------------------------------------------------------------
+
+(use-package reftex
+  :ensure nil  ;; Built-in
+  :defer t
+  :hook (LaTeX-mode . turn-on-reftex)
+  :custom
+  (reftex-enable-partial-scans t)
+  (reftex-save-parse-info t)
+  (reftex-plug-into-AUCTeX t)
+  (reftex-ref-style-default-list '("Default" "AMSmath" "Cleveref"))
+  (reftex-use-multiple-selection-buffers t)
+  (reftex-toc-split-windows-horizontally t)
+  (reftex-toc-split-windows-fraction 0.2))
+
+;; ----------------------------------------------------------------------------
+;; Citar - Bibliographic Management for Org & LaTeX
+;; ----------------------------------------------------------------------------
+
+(use-package citar
+  :ensure t
+  :defer t
+  :custom
+  (org-cite-insert-processor 'citar)
+  (org-cite-follow-processor 'citar)
+  (org-cite-activate-processor 'citar)
+  (citar-bibliography org-cite-global-bibliography))
+
+(use-package citar-embark
+  :ensure t
+  :defer
+  :after (citar embark)
+  :hook (citar-mode . citar-embark-mode)
+  :custom
+  (citar-at-point-function 'embark-act))
+
+;; ----------------------------------------------------------------------------
+;; AUCTeX-Latexmk - Automate LaTeX Compilation
+;; ----------------------------------------------------------------------------
+
+(use-package auctex-latexmk
+  :ensure t
+  :defer t
+  :hook (LaTeX-mode . auctex-latexmk-setup)
+  :custom
+  (auctex-latexmk-inherit-TeX-PDF-mode t))
+
+;; ----------------------------------------------------------------------------
+;; Custom LaTeX Classes for Org Export
+;; ----------------------------------------------------------------------------
+
+(with-eval-after-load 'ox-latex
+  (dolist (class
+           '(("IEEEtran" "\\documentclass[conference]{IEEEtran}"
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+             ("article" "\\documentclass[11pt]{article}"
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+             ("report" "\\documentclass[11pt]{report}"
+              ("\\part{%s}" . "\\part*{%s}")
+              ("\\chapter{%s}" . "\\chapter*{%s}")
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+             ("book" "\\documentclass[11pt]{book}"
+              ("\\part{%s}" . "\\part*{%s}")
+              ("\\chapter{%s}" . "\\chapter*{%s}")
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+    (add-to-list 'org-latex-classes class)))
+
+;; ----------------------------------------------------------------------------
+;; CDLaTeX - Faster LaTeX Input
+;; ----------------------------------------------------------------------------
+
+(use-package cdlatex
+  :ensure t
+  :defer t
+  :hook (LaTeX-mode . turn-on-cdlatex))
+
+
+(provide 'setup-latex)
+;;; setup-latex.el ends here
