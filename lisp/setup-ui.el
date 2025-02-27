644850870eee94fefd81c5696a329cfeb4aaef22
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-02-27 22:16:02 +0530

    create lisp/setup-ui.el

---
+++ lisp/setup-ui.el
@@ -0,0 +1,183 @@
+;; Get rid of the splash screen, and make the *scratch* buffer suitable for
+;; writing.
+(setq inhibit-startup-message t
+      inhibit-splash-screen t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message ";; This buffer is for text that is not saved, and you can enable any mode to code in it.
+;; To create a file, visit it with `\\[find-file]' and enter text in its buffer.
+
+"
+      )
+(fset #'display-startup-echo-area-message #'ignore)
+
+;; Stop the cursor from blinking.
+(blink-cursor-mode 0)
+;; No fat cursors.
+(setq x-stretch-cursor nil)
+
+;; Turn off the menu, tool bars and the scroll bar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+;; Turn on image viewing
+;; (auto-image-file-mode t)
+
+;; Get rid of the annoying system beep.
+(setq ring-bell-function 'ignore)
+
+;; Show me what I type, immediately.
+(setq echo-keystrokes 0.01)
+
+;; Middle-click paste at point, not at cursor.
+(setq mouse-yank-at-point t)
+;; Mouse available in terminal
+;; (add-hook 'tty-setup-hook #'xterm-mouse-mode)
+
+(setq dnd-indicate-insertion-point t
+      dnd-scroll-margin 4)
+
+;; Scrolling
+(setq scroll-margin 0
+      scroll-preserve-screen-position t
+      next-screen-context-lines 3)
+;;; mouse
+;;; (setq mouse-wheel-scroll-amount '(t ((shift) . 2))
+;;;       mouse-wheel-progressive-speed t)
+
+;;; (setq-hook! '(eshell-mode-hook term-mode-hook) hscroll-margin 0)
+
+;; Reduce the clutter in the fringes; we'd like to reserve that space for more
+;; useful information, like git-gutter and flycheck.
+(setq indicate-buffer-boundaries t
+      indicate-empty-lines nil
+      scroll-conservatively 101
+      auto-window-vscroll nil)
+
+;;; remove continuation arrow on right fringe
+;;; (delq! 'continuation fringe-indicator-alist 'assq)
+
+;; Don't resize emacs in steps.
+(setq window-resize-pixelwise t
+      frame-resize-pixelwise t)
+
+;; The native border "consumes" a pixel of the fringe on righter-most splits,
+;; `window-divider' does not. Available since Emacs 25.1.
+;; (setq window-divider-default-places t
+;;       window-divider-default-bottom-width 2
+;;       window-divider-default-right-width 2)
+;; (add-hook 'after-init-hook #'window-divider-mode)
+
+;; No popup dialogs
+(setq use-dialog-box nil
+      use-file-dialog nil
+      pop-up-windows nil)
+
+(tooltip-mode 1)
+;; native linux tooltips are all right
+(when IS-LINUX
+  (setq x-gtk-use-system-tooltips nil))
+
+;; Window splitting
+;; Set horizontal splits as the default
+;; #+begin_src emacs-lisp
+;; (setq split-width-threshold 120
+;;       split-height-threshold 80)
+;; #+end_src
+;; or favor vertical splits over horizontal ones?
+(setq split-width-threshold 140
+      split-height-threshold 80)
+
+;; Some custom settings
+
+(setq user-full-name "Karna"
+      long-line-threshold 100000
+      global-auto-revert-non-file-buffers t
+      size-indication-mode t
+      undo-limit (* 80 1024 1024)
+      locale-coding-system 'utf-8)
+
+(add-hook 'before-save-hook #'whitespace-cleanup)
+(add-hook 'comint-exec-hook (lambda () (set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil)))
+
+(delete-selection-mode 1)
+(electric-indent-mode -1)
+(electric-pair-mode 1)
+(global-visual-line-mode 1)
+(global-hl-line-mode -1)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(global-prettify-symbols-mode 1)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(pixel-scroll-precision-mode -1)
+
+;; 🔥 Custom Keybindings
+(global-set-key (kbd "C-c K") 'kill-some-buffers)
+(global-set-key [escape] 'keyboard-escape-quit)
+
+(add-hook 'after-init-hook #'global-auto-revert-mode)  ;; Auto-refresh buffers when files change
+(add-hook 'after-init-hook #'save-place-mode)          ;; Remember cursor positions in files
+
+;;; Custom Cache and saving file paths 
+
+(setq save-place-file (dir-concat user-cache-directory "places")
+      transient-history-file (dir-concat user-cache-directory "transient-history.el")
+      auto-save-list-file-prefix (dir-concat user-cache-directory "auto-save-list/.saves-")
+      tramp-persistency-file-name (dir-concat user-cache-directory "tramp")
+      lsp-session-file (dir-concat user-cache-directory "lsp-session")
+      dap-breakpoints-file (dir-concat user-cache-directory "dap-breakpoints")
+      eshell-directory-name (dir-concat user-cache-directory "eshell")
+      eshell-history-file-name (dir-concat user-cache-directory "eshell-history")
+      eshell-last-dir-ring-file-name (dir-concat user-cache-directory "eshell-last-dir-ring")
+      persp-state-default-file (dir-concat user-cache-directory "sessions")
+      org-id-locations-file (dir-concat user-cache-directory ".org-id-locations")
+      org-roam-db-location "/mnt/Karna/Git/Project-K/Org/Roam/org-roam.db"
+      preview-tailor-storage-file (dir-concat user-cache-directory ".preview-tailor"))
+
+;;; ;;;###package pos-tip
+;;; (setq pos-tip-internal-border-width 6
+;;;       pos-tip-border-width 1)
+;;; ;; Better fontification of number literals in code
+
+;;; (use-package! highlight-numbers
+;;;   :hook ((prog-mode conf-mode) . highlight-numbers-mode)
+;;;   :config (setq highlight-numbers-generic-regexp "\\_<[[:digit:]]+\\(?:\\.[0-9]*\\)?\\_>"))
+
+;;; ;;;###package hide-mode-line-mode
+;;; (add-hook! '(completion-list-mode-hook Man-mode-hook)
+;;;            #'hide-mode-line-mode)
+
+;; ----------------------------------------------------------------------------
+;; ICONS
+;; ----------------------------------------------------------------------------
+(use-package all-the-icons
+  :ensure t
+  :defer
+  :if (display-graphic-p))
+
+(use-package all-the-icons-dired
+  :ensure t
+  :defer
+  :if (display-graphic-p)
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package all-the-icons-completion
+  :ensure t
+  :defer
+  :if (display-graphic-p)
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :init
+  (all-the-icons-completion-mode))
+
+(use-package nerd-icons
+  :ensure t
+  :defer
+  :if (display-graphic-p)
+  :custom
+  (nerd-icons-color-icons t))
+
+(provide 'setup-ui)
+;; setup-ui ends here
