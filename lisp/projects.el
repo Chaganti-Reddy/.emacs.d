1b47172287605090b11ea4db6afc5338cd99ce98
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-01 15:49:29 +0530

    update lisp/projects.el

--- lisp/projects.el
+++ lisp/projects.el
@@ -8,7 +8,7 @@
 (require 'project)
 
 ;; Default action when switching projects (open in Dired)
-(setq project-switch-commands 'project-dired)
+;; (setq project-switch-commands 'project-dired)
 
 ;; Ignore unnecessary directories
 (setq project-vc-extra-root-markers '(".git" ".hg" ".svn")) ;; Ensure proper detection
@@ -16,13 +16,6 @@
       '("node_modules" "dist" "build" "vendor" ".venv" "tmp" "cache" "log" "bower_components"))
 
 (setq project-list-file (expand-file-name ".cache/projects" user-emacs-directory))
-
-;; Keybindings for easy access
-(global-set-key (kbd "C-c p f") #'project-find-file)        ;; Find file in project
-(global-set-key (kbd "C-c p s") #'project-search)           ;; Search in project
-(global-set-key (kbd "C-c p p") #'project-switch-project)   ;; Switch project
-(global-set-key (kbd "C-c p d") #'project-dired)            ;; Open project root in dired
-
 
 (provide 'projects)
 ;;; projects.el ends here
