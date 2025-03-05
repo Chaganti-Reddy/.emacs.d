29ceed749887059be192a12d5dd5bdd42a983249
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-05 22:44:02 +0530

    update custom.el

--- custom.el
+++ custom.el
@@ -8,10 +8,4 @@
    '("b41d0a9413fb0034cea34eb8c9f89f6e243bdd76bccecf8292eb1fefa42eaf0a"
      "cd5f8f91cc2560c017cc9ec24a9ab637451e36afd22e00a03e08d7b1b87c29ca"
      default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "PfEd" :slant normal :weight medium :height 110 :width normal))))
- '(variable-pitch ((t (:family "Merriweather" :width semi-expanded)))))
+
