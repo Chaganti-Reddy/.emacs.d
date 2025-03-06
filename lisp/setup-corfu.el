4c342032cf704204f2786d78c26b6fa6b500243c
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-06 11:56:09 +0530

    update lisp/setup-corfu.el

--- lisp/setup-corfu.el
+++ lisp/setup-corfu.el
@@ -10,7 +10,8 @@
   :defer t
   :hook ((prog-mode . corfu-mode)
          ;; (org-mode . corfu-mode)
-         (latex-mode . corfu-mode))
+         ;; (LaTeX-mode . corfu-mode)
+         )
   :custom
   (corfu-cycle t)                  ;; Enable cycling through candidates
   (corfu-auto t)                   ;; Auto-show completions
