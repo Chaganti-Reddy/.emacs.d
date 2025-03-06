ebc0062cadc99160db1c404ba12dee9ed0a84c60
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-06 20:48:01 +0530

    update lisp/projects.el

--- lisp/projects.el
+++ lisp/projects.el
@@ -17,5 +17,27 @@
 
 (setq project-list-file (expand-file-name ".cache/projects" user-emacs-directory))
 
+
+;; The [[https://github.com/karthink/project-x][project-x]] package adds support
+;; for "local" projects, as well as facilities for periodically saving
+;; project-specific window configurations. It also adds commands to save and
+;; restore project windows.
+
+
+(use-package project-x
+  :load-path "~/.emacs.d/plugins/project-x.el"
+  :ensure nil
+  ;; :ensure (:host github :protocol ssh
+  ;;            :repo "karthink/project-x")
+  :after project
+  :config
+  (setq project-x-local-identifier '(".project"))
+  (setq project-x-window-list-file (dir-concat user-cache-directory "project-window-list")
+        project-x-save-interval nil)
+  (project-x-mode 1))
+
+(add-hook 'project-find-functions 'project-x-try-local 90)
+    
+
 (provide 'projects)
 ;;; projects.el ends here
