b038100ede92c05a0c6d50f9a1d0307379dfe4ba
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-07 15:13:11 +0530

    update custom.el

--- custom.el
+++ custom.el
@@ -7,5 +7,19 @@
  '(custom-safe-themes
    '("b41d0a9413fb0034cea34eb8c9f89f6e243bdd76bccecf8292eb1fefa42eaf0a"
      "cd5f8f91cc2560c017cc9ec24a9ab637451e36afd22e00a03e08d7b1b87c29ca"
-     default)))
+     default))
+ '(project-list-file "/home/karna/.emacs.d/.cache/projects"))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t :slant italic :weight semibold)))
+ '(font-lock-function-name-face ((t :weight bold)))
+ '(font-lock-keyword-face ((t :slant italic :weight semibold)))
+ '(font-lock-type-face ((t :weight bold)))
+ '(outline-1 ((((class color) (min-colors 256)) :height 1.25)))
+ '(outline-2 ((((class color) (min-colors 256)) :height 1.2)))
+ '(outline-3 ((((class color) (min-colors 256)) :height 1.16)))
+ '(outline-4 ((((class color) (min-colors 256)) :height 1.12))))
