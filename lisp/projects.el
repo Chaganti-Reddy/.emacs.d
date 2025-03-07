ddbee1f77a2cfd4e2274263eb42ba6921926241b
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2025-03-07 15:13:11 +0530

    update lisp/projects.el

--- lisp/projects.el
+++ lisp/projects.el
@@ -37,6 +37,33 @@
   (project-x-mode 1))
 
 (add-hook 'project-find-functions 'project-x-try-local 90)
+
+
+(defun karna/clean-project-known-projects ()
+  "Remove non-existing paths from the project list and persist changes."
+  (interactive)
+  (when (boundp 'project--list)
+    (setq project--list
+          (seq-filter (lambda (proj)
+                        (file-directory-p (if (consp proj) (car proj) proj)))
+                      project--list)))
+  
+  ;; Remove stale projects using project-forget-project
+  (when (fboundp 'project-forget-project)
+    (dolist (proj (copy-sequence (project-known-project-roots)))  ;; Avoid mutation issues
+      (unless (file-directory-p proj)
+        (project-forget-project proj))))  ;; Forget only if the directory doesn't exist
+
+  ;; Persist the updated project list
+  (when (and (boundp 'project-list-file) project-list-file)
+    (with-temp-file project-list-file
+      (insert (prin1-to-string project--list))))
+
+  (message "Removed stale project paths and saved changes to %s" project-list-file))
+
+(global-set-key (kbd "C-x p C") #'karna/clean-project-known-projects)
+
+;; (add-hook 'emacs-startup-hook #'karna/clean-project-known-projects)
     
 
 (provide 'projects)
